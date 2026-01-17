1ac137f2c91acd1e548caf7331affd55f13aa197
Bougarra <bougarra.02@gmail.com>
2026-01-17 23:31:09 +0100

    Refactoring Code

--- script.py
+++ script.py
@@ -10,9 +10,6 @@ from imblearn.over_sampling import SMOTE
 
 from PIL import Image 
 
-from PIL import Image
-
-
 ## skelarn -- preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
@@ -20,14 +17,10 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn_features.transformers import DataFrameSelector
 
-## skelarn -- models
-# from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-# from sklearn.linear_model import LogisticRegression
 
 ## sklearn -- metrics
 from sklearn.metrics import f1_score, confusion_matrix
-
 
 ## --------------------- Data Preparation ---------------------------- ##
 
@@ -41,14 +34,12 @@ df.drop(columns=['RowNumber', 'CustomerId', 'Surname'], axis=1, inplace=True)
 ## Filtering using Age Feature using threshold
 df.drop(index=df[df['Age'] > 80].index.tolist(), axis=0, inplace=True)
 
-
 ## To features and target
 X = df.drop(columns=['Exited'], axis=1)
 y = df['Exited']
 
 ## Split to train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=45, stratify=y)
-
 
 ## --------------------- Data Processing ---------------------------- ##
 
@@ -58,14 +49,12 @@ categ_cols = ['Gender', 'Geography']
 
 ready_cols = list(set(X_train.columns.tolist()) - set(num_cols) - set(categ_cols))
 
-
 ## For Numerical
 num_pipeline = Pipeline(steps=[
                         ('selector', DataFrameSelector(num_cols)),
                         ('imputer', SimpleImputer(strategy='median')),
                         ('scaler', StandardScaler())
                     ])
-
 
 ## For Categorical
 categ_pipeline = Pipeline(steps=[
@@ -74,14 +63,11 @@ categ_pipeline = Pipeline(steps=[
                         ('ohe', OneHotEncoder(drop='first', sparse_output=False))
                     ])
 
-
 ## For ready cols
 ready_pipeline = Pipeline(steps=[
                         ('selector', DataFrameSelector(ready_cols)),
                         ('imputer', SimpleImputer(strategy='most_frequent'))
                     ])
-
-
 
 ## combine all
 all_pipeline = FeatureUnion(transformer_list=[
@@ -94,7 +80,6 @@ all_pipeline = FeatureUnion(transformer_list=[
 X_train_final = all_pipeline.fit_transform(X_train)
 X_test_final = all_pipeline.transform(X_test)
 
-
 ## --------------------- Impalancing ---------------------------- ##
 
 ## 1. use algorithm without taking the effect of imbalancing
@@ -102,7 +87,6 @@ X_test_final = all_pipeline.transform(X_test)
 ## 2. prepare class_weights for solving imbalance dataset
 vals_count = 1 - (np.bincount(y_train) / len(y_train))
 vals_count = vals_count / np.sum(vals_count)  ## normalizing
-
 
 dict_weights = {}
 for i in range(2):  ## 2 classes (0, 1)
@@ -112,19 +96,15 @@ for i in range(2):  ## 2 classes (0, 1)
 over = SMOTE(sampling_strategy=0.7)
 X_train_resmapled, y_train_resampled = over.fit_resample(X_train_final, y_train)
 
-
 ## --------------------- Modeling ---------------------------- ##
 
 ## Clear metrics.txt file at the beginning
 with open('metrics.txt', 'w') as f:
     pass
 
-
 def train_model(X_train, y_train, plot_name='', class_weight=None):
     """ A function to train model given the required train data """
-    
     global clf_name
-
 
     clf = RandomForestClassifier(n_estimators=3000, max_depth=10, random_state=45, class_weight=class_weight)
 
@@ -149,7 +129,6 @@ def train_model(X_train, y_train, plot_name='', class_weight=None):
     plt.savefig(f'{plot_name}.png', bbox_inches='tight', dpi=300)
     plt.close()
 
-
     ## Write scores to a file
     with open('metrics.txt', 'a') as f:
             f.write(f'{clf_name} {plot_name}\n')
@@ -157,9 +136,7 @@ def train_model(X_train, y_train, plot_name='', class_weight=None):
             f.write(f"F1-Score of Validation is: {score_test*100:.2f} %\n")
             f.write('----'*10 + '\n')
 
-
     return True
-
 
 ## 1. without considering the imabalancing data
 train_model(X_train=X_train_final, y_train=y_train, plot_name='without-imbalance', class_weight=None)
@@ -169,8 +146,6 @@ train_model(X_train=X_train_final, y_train=y_train, plot_name='with-class-weight
 
 ## 3. with considering the imabalancing data using oversampled data (SMOTE)
 train_model(X_train=X_train_resmapled, y_train=y_train_resampled, plot_name=f'with-SMOTE', class_weight=None)
-
-
 
 ## Combine all conf matrix in one
 confusion_matrix_paths = [f'./without-imbalance.png', f'./with-class-weights.png', f'./with-SMOTE.png']
@@ -183,7 +158,6 @@ for i, path in enumerate(confusion_matrix_paths, 1):
     plt.imshow(img)
     plt.axis('off')  # Disable axis for cleaner visualization
 
-
 ## Save combined plot locally
 plt.suptitle(clf_name, fontsize=16)
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
@@ -191,4 +165,4 @@ plt.savefig(f'conf_matrix.png', bbox_inches='tight', dpi=300)
 
 ## Delete old image files
 ## for path in confusion_matrix_paths:
-    ##os.remove(path)
+##      os.remove(path)
